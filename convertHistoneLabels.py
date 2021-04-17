f18ec3995bcade52672043a6770be2269a4d249f
Alex Ruan <alexruan2222@gmail.com>
2021-04-16 23:08:45 -0400

    added tokenized data

---
+++ convertHistoneLabels.py
@@ -0,0 +1,46 @@
+# Script to convert labels into categories based on arguments
+
+import argparse
+import numpy as np
+import pandas as pd
+
+
+# Example command: python3 convertHistoneLabels.py --cell_file data/Cell1.test.csv --output_file Cell1Conv.test.csv
+
+def convertLabels(cell_file, output_file):
+# From find_features()
+    histones = None
+
+    # Read in histone modification data from text files
+
+    with open(cell_file, "r") as text_file:
+        histones_lines = text_file.read().splitlines()
+        histones = np.asarray([np.asarray(line.split(",")[1:]) for line in histones_lines])
+
+    #Convert numpy matrix to pandas dataframe
+    df = pd.DataFrame(histones)
+    df = df.apply(pd.to_numeric)
+    df.columns = ["hm1", "hm2", "hm3", "hm4", "hm5"]
+    
+    allowed_vals = [0,1]
+    df[~df.isin(allowed_vals)] = "h"
+    df = df.replace(0, 'l')
+    df = df.replace(1, "m")
+
+    def concat_row(row):
+        return row["hm1"] + row["hm2"] + row["hm3"] + row["hm4"] + row["hm5"]
+
+    df['concat'] = df.apply (lambda row: concat_row(row), axis=1)
+
+    df['concat'].to_csv(output_file, index=False, header=False)
+
+parser = argparse.ArgumentParser(description='CreateLabels')
+parser.add_argument('--cell_file', type=str, default='data/Cell1.expr.csv', help='Path to file for cell 1')
+parser.add_argument('--output_file', type=str, default="output_cell.csv", help="path for output file")
+
+args = parser.parse_args()
+
+convertLabels(args.cell_file, args.output_file)
+
+
+
--- data_exploration.py
+++ data_exploration.py
@@ -61,4 +61,53 @@ def find_expression():
     plt.suptitle("log(cell1 expression + 1) - log(cell2 expression + 1)")
     plt.show()
 
-find_expression()
+def find_ranges():
+    # From find_features()
+    train, test, valid = None, None, None
+
+    # Read in histone modification data from text files
+    with open("data/Cell1.train.csv", "r") as text_file:
+        train_lines = text_file.read().splitlines()
+        train = np.asarray([np.asarray(line.split(",")[1:])for line in train_lines])
+
+    with open("data/Cell1.test.csv", "r") as text_file:
+        test_lines = text_file.read().splitlines()
+        test = np.asarray([np.asarray(line.split(",")[1:])for line in test_lines])
+
+    with open("data/Cell1.valid.csv", "r") as text_file:
+        valid_lines = text_file.read().splitlines()
+        valid = np.asarray([np.asarray(line.split(",")[1:])for line in valid_lines])
+
+    #Concatenate data into single numpy matrix
+    Cell1_concat = np.concatenate((train, test, valid), axis=0)
+
+    #Convert numpy matrix to pandas dataframe
+    df = pd.DataFrame(Cell1_concat)
+    df = df.apply(pd.to_numeric)
+    df.columns = ["hm1", "hm2", "hm3", "hm4", "hm5"]
+
+    hm1 = df["hm1"].tolist()
+    hm2 = df["hm2"].tolist()
+    hm3 = df["hm3"].tolist()
+    hm4 = df["hm4"].tolist()
+    hm5 = df["hm5"].tolist()
+
+    hm1.sort()
+    hm2.sort()
+    hm3.sort()
+    hm4.sort()
+    hm5.sort()
+
+    def find_thresholds(hm_list):
+        print(hm_list[int(len(hm_list)/3.0)], hm_list[int(2*len(hm_list)/3.0)])
+
+    def find_percentiles(hm_list):
+        print(hm_list.index(1)/float(len(hm_list)), hm_list.index(2)/float(len(hm_list)))
+    
+    find_percentiles(hm1)
+    find_percentiles(hm2)
+    find_percentiles(hm3)
+    find_percentiles(hm4)
+    find_percentiles(hm5)
+
+find_ranges()
